f3f439d5e1ddd5575dbc5f0785b1d993863146d6
Aula Nur Rizal Ardiyantoro <rizalardiyantoro123@gmail.com>
2022-12-01 06:23:16 +0700

    (feature): add heropatterns

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,5 +12,36 @@ module.exports = {
     require('@tailwindcss/line-clamp'),
     require('@tailwindcss/typography'),
     require("daisyui"),
+    require("tailwind-heropatterns")({
+      // as per tailwind docs you can pass variants
+      variants: [],
+    
+      // the list of patterns you want to generate a class for
+      // the names must be in kebab-case
+      // an empty array will generate all 87 patterns
+      patterns: ["skulls", "polka-dots", "overlapping-circles", "bubbles", "wiggle", "bathroom-floor"],
+    
+      // The foreground colors of the pattern
+      colors: {
+        default: "#9C92AC",
+        "blue-dark": "#000044", //also works with rgb(0,0,205)
+        "cyan": "#47d7ed",
+        "donker": "#6497b1",
+        "orange": "#f5a623",
+      },
+    
+      // The foreground opacity
+      opacity: {
+        default: "0.3",
+        "100": "1.0",
+        "50": "0.5",
+        "25": "0.25",
+        "10": "0.1",
+      },
+
+      foreground: {
+        default: "#9C92AC",
+      }
+    }),
   ],
 }
