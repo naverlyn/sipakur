4a8d3d3bf48833ddd39a8760c8d9b24374b61d9a
Aula Nur Rizal Ardiyantoro <rizalardiyantoro123@gmail.com>
2022-12-06 23:29:35 +0700

    (add): cards

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,5 @@
 import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 import Head from "next/head";
 
 
@@ -11,12 +12,62 @@ export default function About() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <NavBar />
-            <div className="w-full h-screen flex flex-col bg-hero-overlapping-circles-donker bg-cyan-100">
-                <div className="container mx-auto px-6 py-8">
-                    <h3 className="text-gray-700 text-2xl font-medium">About Page</h3>
-                    <span className="mt-3 text-sm text-gray-500">SICAKUR | About</span>
+            <div className="w-screen h-screen flex flex-col">
+                <div className="container">
+                    <div className="flex flex-col items-center justify-center w-full h-full">
+                        <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+                            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                                <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                                <div className="card-body">
+                                    <h2 className="card-title">Wita</h2>
+                                    <p className="text-gray-700 text-xs">
+                                        Ketua Kelompok Pengantar Intelegensi Buatan
+                                    </p>
+                                    <p className="text-gray-700 text-base">Hobi saya adalah membaca dan mendengarkan musik.</p>
+                                    <div class="px-0 pt-4 pb-2">
+                                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#music</span>
+                                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#reading</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+                            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                                <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                                <div className="card-body">
+                                    <h2 className="card-title">Septian Nugraha Kartadilaga</h2>
+                                    <p class="text-gray-700 text-xs">
+                                        Anggota
+                                    </p>
+                                    <p class="text-gray-700 text-base">
+                                        Saya senang bermain futsal.
+                                    </p>
+                                    <div class="px-0 pt-4 pb-2">
+                                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#soccer</span>
+                                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#sport</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                                <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                                <div className="card-body">
+                                    <h2 className="card-title">Jawaril Haq Al-Azkiya</h2>
+                                    <p class="text-gray-700 text-xs">
+                                        Anggota
+                                    </p>
+                                    <p class="text-gray-700 text-xs">
+                                        Ngorok bossssssssssssssssss
+                                    </p>
+                                    <div class="px-0 pt-4 pb-2">
+                                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#sleeping</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+            <Footer />
         </>
     );
 }
