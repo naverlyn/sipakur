26d30e2b12aff80d2b2212968a3cbf0db3406a56
Aula Nur Rizal Ardiyantoro <rizalardiyantoro123@gmail.com>
2022-12-06 23:28:23 +0700

    (update): precise list of person

--- services/InferenceService.js
+++ services/InferenceService.js
@@ -2,14 +2,12 @@ export default class ManInferenceService
 {
     static manInferenceCalculator(tinggi, dada)
     {
-        if ((tinggi >= 166 && tinggi <= 170) && (dada >= 86 && dada <= 90)) return 44;
-        if ((tinggi >= 168 && tinggi <= 173) && (dada >= 90 && dada <= 93)) return 46;
-        if ((tinggi >= 171 && tinggi <= 176) && (dada >= 94 && dada <= 97)) return 48;
-        if ((tinggi >= 174 && tinggi <= 179) && (dada >= 98 && dada <= 101)) return 50;
-        if ((tinggi >= 177 && tinggi <= 182) && (dada >= 102 && dada <= 105)) return 52;
-        if ((tinggi >= 180 && tinggi <= 184) && (dada >= 106 && dada <= 109)) return 54;
-        if ((tinggi >= 182 && tinggi <= 186) && (dada >= 110 && dada <= 113)) return 56;
-        if ((tinggi >= 184 && tinggi <= 188) && (dada >= 114 && dada <= 117)) return 58;
+        if ((tinggi >= 166 && tinggi <= 170) && (dada >= 48 && dada <= 52)) return 44;
+        if ((tinggi >= 168 && tinggi <= 173) && (dada >= 53 && dada <= 58)) return 48;
+        if ((tinggi >= 174 && tinggi <= 179) && (dada >= 59 && dada <= 64)) return 52;
+        if ((tinggi >= 177 && tinggi <= 182) && (dada >= 65 && dada <= 71)) return 56;
+        if ((tinggi >= 180 && tinggi <= 184) && (dada >= 72 && dada <= 75)) return 60;
+        if ((tinggi >= 185 && tinggi <= 190) && (dada >= 76 && dada <= 80)) return 63;
         return 0;
     };
 
@@ -19,31 +17,31 @@ export default class ManInferenceService
         if(inference >= 48 && inference <= 50) return "M";
         if(inference >= 52 && inference <= 54) return "L";
         if(inference >= 56 && inference <= 58) return "XL";
+        if(inference >= 60 && inference <= 62) return "XXL";  
+        if(inference > 63)
+         return "XXXL";      
         return "Tidak ada";
     };
 
     static womanInferenceCalculator(dada, pinggang)
     {
-        if ((dada >= 74 && dada <= 77) && (pinggang >= 60 && pinggang <= 62)) return 32;
-        if ((dada >= 78 && dada <= 81) && (pinggang >= 63 && pinggang <= 65)) return 34;
-        if ((dada >= 82 && dada <= 85) && (pinggang >= 66 && pinggang <= 69)) return 36;
-        if ((dada >= 86 && dada <= 89) && (pinggang >= 70 && pinggang <= 73)) return 38;
-        if ((dada >= 90 && dada <= 93) && (pinggang >= 74 && pinggang <= 77)) return 40;
-        if ((dada >= 94 && dada <= 97) && (pinggang >= 78 && pinggang <= 81)) return 42;
-        if ((dada >= 98 && dada <= 102) && (pinggang >= 82 && pinggang <= 86)) return 44;
-        if ((dada >= 103 && dada <= 107) && (pinggang >= 87 && pinggang <= 91)) return 46;
-        if ((dada >= 108 && dada <= 113) && (pinggang >= 92 && pinggang <= 96)) return 48;
-        if ((dada >= 114 && dada <= 119) && (pinggang >= 97 && pinggang <= 102)) return 50;
+        if ((dada >= 82 && dada <= 86) && (pinggang >= 59 && pinggang <= 63)) return 36;
+        if ((dada >= 86 && dada <= 90) && (pinggang >= 63 && pinggang <= 67)) return 38;
+        if ((dada >= 90 && dada <= 94) && (pinggang >= 67 && pinggang <= 71)) return 40;
+        if ((dada >= 94 && dada <= 98) && (pinggang >= 71 && pinggang <= 75)) return 42;
+        if ((dada >= 102 && dada <= 106) && (pinggang >= 79 && pinggang <= 83)) return 46;
+        if  ((dada > 106) && (pinggang > 83)) return 51;
         return 0;
     };
 
     static womanSizeCalculator(inference)
     {
-        if(inference >= 32 && inference <= 34) return "XS";
         if(inference >= 36 && inference <= 38) return "S";
-        if(inference >= 40 && inference <= 42) return "M";
-        if(inference >= 44 && inference <= 46) return "L";
-        if(inference >= 48 && inference <= 50) return "XL";
+        if(inference >= 38 && inference <= 40) return "M";
+        if(inference >= 40 && inference <= 42) return "L";
+        if(inference >= 42 && inference <= 44) return "Xl";
+        if(inference >= 46 && inference <= 50) return "XXL";
+        if(inference > 51) return "XXXL";
         return "Tidak ada";
     };
 }
