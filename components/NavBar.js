65b4cf106a49a6d652e0745ee0e1a3a319ca103a
Aula Nur Rizal Ardiyantoro <rizalardiyantoro123@gmail.com>
2022-12-15 12:39:19 +0700

    (fix): link

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,3 +1,6 @@
+import Link from "next/link"
+
+
 
 export default function NavBar() {
     return (
@@ -5,7 +8,7 @@ export default function NavBar() {
             <div className="navbar bg-white/40 backdrop-blur-sm sticky left-0 top-0 z-[9999] shadow-lg rounded-xl">
                 <div className="max-w-10xl w-full mx-auto px-6 text-xl font-bold">
                     <div className="flex justify-between">
-                        <a className="text-lg font-bold btn btn-ghost" href="/">SIPAKUR</a>
+                        <Link className="text-lg font-bold btn btn-ghost" href="/">SIPAKUR</Link>
                     </div>
                 </div>
                 <div className="flex justify-end flex-1 px-2">
@@ -13,7 +16,7 @@ export default function NavBar() {
                         <div className="dropdown dropdown-end">
                             <label tabIndex={0} className="btn btn-ghost rounded-btn">Menu</label>
                             <ul tabIndex={0} className="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52 mt-4">
-                                <li><a href="/about">About</a></li>
+                                <li><Link href="/about">About</Link></li>
                             </ul>
                         </div>
                     </div>
